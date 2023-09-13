e012e1f0b7788f7d222c7f0cc6babe6ea42f63c2
Alejo Chavarri <alechavarri89@gmail.com>
2023-09-13 12:02:15 -0300

    Buscar, listar y mostrar detalle funcionando

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Dominio;
+using Server;
 namespace WindowsFormsApp
 {
     public partial class Form1 : Form
+
     {
+        private List<Articulos> Listarticulos;
+
+        private void mostrarDetalle (Articulos A)
+        {
+            MessageBox.Show(A.ToString());
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArticuloServer DB = new ArticuloServer();
+            Listarticulos = DB.listar();
+            dgbArticulos.DataSource = Listarticulos;
+            
+            
+        }
+
+        private void verCatalogoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ArticuloServer DB = new ArticuloServer();
+            Listarticulos = DB.listar();
+            dgbArticulos.DataSource = Listarticulos;
+        }
+
+        private void dgbArticulos_DoubleClick(object sender, EventArgs e)
+        {
+            Articulos A;
+            A = (Articulos)dgbArticulos.CurrentRow.DataBoundItem;
+            MessageBox.Show(A.ToString());
+        }
+
+        private void dgbArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+            Articulos A;
+            A = (Articulos)dgbArticulos.CurrentRow.DataBoundItem;
+            pictureBox1.Load(A.linkImagen);
+        }
+
+        private void buscarArticuloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String Codigo = Microsoft.VisualBasic.Interaction.InputBox("Bucar", "Codigo de Articulo", "");
+            Articulos A;
+            bool flag = false;
+            for (int i = 0; i < Listarticulos.Count(); i++)
+            {
+                A = Listarticulos[i];
+                if (A.Codigo.Equals(Codigo))
+                {
+                    MessageBox.Show(A.ToString());
+                    flag = true;
+                }
+                
+            }
+            if (!flag)
+            {
+                MessageBox.Show("No se encontro el articulo, reintente mas tarde ... ");
+            }
         }
     }
 }
