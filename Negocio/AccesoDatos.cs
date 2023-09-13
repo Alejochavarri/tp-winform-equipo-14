3b92f9cea6d75b2b9af6c39d995d088c17be3ca7
LeonasZ <106259939+LeonasZ@users.noreply.github.com>
2023-09-13 16:58:35 -0300

    Add files via upload

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector 
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database= CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            try
+            {
+                if (lector != null)
+                    lector.Close();
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+    }
+}
